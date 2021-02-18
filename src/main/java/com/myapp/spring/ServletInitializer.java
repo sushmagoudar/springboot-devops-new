11a6049a41709aae4b7aee5f86ed78ddd35fc931
sushmagoudar <goudarsushma11@gmail.com>
2021-02-18 13:10:07 +0530

    commit

--- src/main/java/com/myapp/spring/ServletInitializer.java
+++ src/main/java/com/myapp/spring/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringDevopsApplication.class);
+		return application.sources(SpringbootDevopsNewApplication.class);
 	}
 
 }
